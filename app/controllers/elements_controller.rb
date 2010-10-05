3767260a41cb11435f43be05e9fed8ded3b83f55
Jon Smith <jonathan@impacti.org>
2010-10-05 21:11:22 +0100

    First commit

---
+++ app/controllers/elements_controller.rb
@@ -0,0 +1,107 @@
+class ElementsController < ApplicationController
+
+  # GET /elements
+  # GET /elements.xml
+  def index
+    @elements = Element.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @elements }
+    end
+  end
+
+  # GET /elements/1
+  # GET /elements/1.xml
+  def show
+    @element = Element.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @element }
+    end
+  end
+
+  # GET /elements/new
+  # GET /elements/new.xml
+  def new
+    @element = Element.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @element }
+    end
+  end
+
+  # GET /elements/1/edit
+  def edit
+    @element = Element.find(params[:id])
+  end
+
+  # POST /elements
+  # POST /elements.xml
+  def create
+    @element = Element.new(params[:element])
+
+    respond_to do |format|
+      if @element.save
+        format.html { redirect_to(@element, :notice => 'Element was successfully created.') }
+        format.xml  { render :xml => @element, :status => :created, :location => @element }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @element.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /elements/1
+  # PUT /elements/1.xml
+  def update
+    @element = Element.find(params[:id])
+
+    respond_to do |format|
+      if @element.update_attributes(params[:element])
+        format.html { redirect_to(@element, :notice => 'Element was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @element.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /elements/1
+  # DELETE /elements/1.xml
+  def destroy
+    @element = Element.find(params[:id])
+    @element.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(elements_url) }
+      format.xml  { head :ok }
+    end
+  end
+  
+  include HTTParty
+  
+  def listposts
+    @response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
+  end
+  
+end
+
+# class ConsumeController < ApplicationController
+# 
+#   include HTTParty
+#   format :json
+#   base_uri 'impacti-org.socialcast.com'
+#   basic_auth 'jonathan@impacti.org', 'impactful'
+# 
+#   def fetch
+#     @response = "hello"
+#     response = Consume.get('/api/messages.json')
+#         response['messages'].each do |item|
+#           puts item['body'][0..11] +" <> "+ item['id'].to_s
+#         end
+#   end
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digestable::Application.config.secret_token = 'a9044161daf5faa70d513892a5ab08838395b229ded24cd759799bea235ec14acb3f612f4575edd9c6e53d0e8cacc38cf75dda4cc0afe8fc5e31236ccfce9e9e'
