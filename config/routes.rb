1e548c6545b40ceb95b9f2b766fad843d1d81f18
onlyjsmith <jonathan@impacti.org>
2010-10-11 02:50:57 -0700

    Changed root route to point to listposts

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Digestable::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "elements#index"
+  root :to => "elements#listposts"
 
   # See how all your routes lay out with "rake routes"
 
