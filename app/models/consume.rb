737bd9476d6fee319f425e45c17b071df9a5142d
Jonathan <jonathan@impacti.org>
2010-10-22 01:48:30 +0100

    Successfully DRYed the authentication. Looks a bit dodgy though

--- app/models/consume.rb
+++ app/models/consume.rb
@@ -4,28 +4,33 @@ class Consume
   extend ActiveModel::Naming
 
   attr_accessor :messages, :users, :groups
-    @auth = {:username => 'jonathan@impacti.org', :password => 'impactful'}
-    @options = {:basic_auth => @auth}
 
+  def auth
+    @root_url = "impacti-org"
+    @auth = {:username => "jonathan@impacti.org", :password => 'impactful'}
+    @options = {:basic_auth => @auth}
+  end
 
 
   def get_messages
+    auth
     recentness = (Time.now - 720000).to_i
-    address = "https://impacti-org.socialcast.com/api/messages.json" #?since=#{recentness}"
+    address = "https://#{@root_url}.socialcast.com/api/messages.json" #?since=#{recentness}"
     @messages = HTTParty.get(address, @options)
   end
   
   def get_users
+    auth unless @options
     # @auth = {:username => 'jonathan@impacti.org', :password => 'impactful'}
     # options = {:basic_auth => @auth}
-    address = "https://impacti-org.socialcast.com/api/users.json"
+    address = "https://#{@root_url}.socialcast.com/api/users.json"
     @users = HTTParty.get(address, @options)
   end
   
   def get_groups
     # @auth = {:username => 'jonathan@impacti.org', :password => 'impactful'}
     # options = {:basic_auth => @auth}
-    address = "https://impacti-org.socialcast.com/api/group_memberships.json"
+    address = "https://#{@root_url}.socialcast.com/api/group_memberships.json"
     @groups = HTTParty.get(address, @options)
   end
   
